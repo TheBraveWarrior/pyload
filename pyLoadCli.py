d6cad425ced79eb9a84b0fcf5c7104a9f5e4b6a3
Patrick Scherrer <pedischerrer@gmail.com>
2017-09-12 20:15:14 +0200

    fix for freebsd

--- pyLoadCli.py
+++ pyLoadCli.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/local/bin/python2
 # -*- coding: utf-8 -*-
 #
 #Copyright (C) 2011 RaNaN
--- pyLoadCore.py
+++ pyLoadCore.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/local/bin/python2
 # -*- coding: utf-8 -*-
 """
     This program is free software; you can redistribute it and/or modify
@@ -188,6 +188,7 @@ class Core(object):
         f = open(self.pidfile, "wb")
         f.write(str(pid))
         f.close()
+        os.chmod(self.pidfile, 0660)
 
     def deletePidFile(self):
         if self.checkPidFile():
